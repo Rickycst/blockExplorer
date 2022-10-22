3d9e08cc217c46424638f3a32d2bb8fcee156073
Rickycst <creusot.aymeric7@gmail.com>
2022-10-22 23:33:07 +0200

    Retrieve info on the screen

--- src/eth.py
+++ src/eth.py
@@ -11,4 +11,4 @@ class eth:
 
     def get_transaction(self, trx_hash):
         # 0x5c504ed432cb51138bcf09aa5e8a410dd4a1e204ef84bfed1be16dfba1b22060
-        return web3.eth.get_transaction(trx_hash)
+        return vars(web3.eth.get_transaction(trx_hash))
--- views/transactions.py
+++ views/transactions.py
@@ -6,12 +6,16 @@ from src.eth import  eth
 
 def render_transaction_view(id):
     _eth = eth()
-    id = request.args.get("id")
-    print(id)
-    
+    id = request.args.get("id")    
 
-    if id != None:
-        print(_eth.get_transaction( id))
-        return render_template('transaction.html', blockInfo={})
+    if id:
+        try:
+            print(id)
+            print(_eth.get_transaction( id))
+            return render_template('transaction.html', transactionInfo=_eth.get_transaction(id))
+        except:
+            print('mauvais transaction hash')
+            return render_template('transaction.html',)
+        
     else:
         return render_template('transaction.html',)
