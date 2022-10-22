3cc137dcc8c986c70e569f4eee8ea283c0743709
Rickycst <creusot.aymeric7@gmail.com>
2022-10-22 23:11:14 +0200

    Handle treansaction but only print it in terminal

--- views/transactions.py
+++ views/transactions.py
@@ -1,5 +1,17 @@
-
 from flask import render_template
+from flask import request
+
+from src.eth import  eth
+
 
 def render_transaction_view(id):
-    return render_template('transaction.html')
+    _eth = eth()
+    id = request.args.get("id")
+    print(id)
+    
+
+    if id != None:
+        print(_eth.get_transaction( id))
+        return render_template('transaction.html', blockInfo={})
+    else:
+        return render_template('transaction.html',)
